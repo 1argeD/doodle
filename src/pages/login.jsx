c90ef2689c9e077afd6ea57e5534b2f54483c21c
Username <Email address>
2023-02-26 18:01:55 +0900

    깃 이그노어와 로그인기능 수정

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Background from "../components/Background";
 import Header from '../components/GlobalHeader';
 import LoginBox from "../components/LoginBox";
-import KakaoButton from "../components/element/button/KakaoButton"
 
 const Login = () => {
     return (
         <>  
             <Header IconType={"ArrowBack"}/>
             <Background />
-            <LoginBox>
-            <KakaoButton />
-            </LoginBox>
+            <LoginBox />
         </>
     )
 }
